f5099674b7db7435d281b051fe1fecadd221db87
PoolRG <poolrojasgomez@gmail.com>
2020-12-07 10:25:54 -0500

    ..

---
+++ PortalBuyPlay/PortalBuyPlay/Controllers/BuyController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace PortalBuyPlay.Controllers
+{
+    public class BuyController : Controller
+    {
+        const string conexion = "server=;database=Negocios2020;uid=sa;pwd=sql";
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
